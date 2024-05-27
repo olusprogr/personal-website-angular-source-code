26ffcff7ebe772579a8677ddba50c26fb9f91599
olusprogr <olus.main333@gmail.com>
2024-05-27 20:47:28 +0200

    fixed hashlocationstrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding(), withHashLocation())
   ]
 };
